6235b90de44ff458e39c0af5db6f831f81c13cea
MINJI <always0524@naver.com>
2025-08-28 11:03:01 +0900

    fix. 이전 요청 중단 안되던 문제 해결 - 원인: IIFE 내부에 클린업 함수 작성한 것 때문

--- 20-fetching-data/src/main.jsx
+++ 20-fetching-data/src/main.jsx
@@ -3,7 +3,21 @@ import { createRoot } from 'react-dom/client'
 import App from './app'
 import './styles/main.css'
 
+// class RootApp extends Component {
+//   state = {
+//     name: 'root app',
+//   }
+
+//   render() {
+//     return this.props.children
+//   }
+// }
+
 const root = document.getElementById('root')
 if (!root) throw new Error('문서에 #root 요소가 존재하지 않습니다.')
 
-createRoot(root).render(<App />)
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
