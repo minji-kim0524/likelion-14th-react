ded2de5ce359c357fdfc623136ff838148a37c28
MINJI <always0524@naver.com>
2025-08-29 14:53:19 +0900

    add. 25-hook-of-rules 실습 폴더

---
+++ 25-hook-of-rules/src/app.jsx
@@ -0,0 +1,10 @@
+import { LearnSection } from '@/components'
+import RandomCountUp from '@/demo/app'
+
+export default function App() {
+  return (
+    <LearnSection title="리액트 훅의 규칙">
+      <RandomCountUp />
+    </LearnSection>
+  )
+}
