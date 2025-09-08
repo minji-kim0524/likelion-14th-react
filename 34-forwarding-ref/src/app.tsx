d977fc87a49c765217d4b70c21512fff3dcf53e9
MINJI <always0524@naver.com>
2025-09-08 16:35:38 +0900

    learn. 리액트 19 버전부터 ref는 속성으로 취급

--- 34-forwarding-ref/src/app.tsx
+++ 34-forwarding-ref/src/app.tsx
@@ -1,7 +1,7 @@
 import { type FormEvent, useRef, version } from 'react'
 import { Send } from 'lucide-react'
 import { LearnSection } from '@/components'
-import EmailInput from './components/form/email-input-remind'
+import EmailInput from './components/form/email-input'
 
 export default function App() {
   const inputRef = useRef<HTMLInputElement>(null)
@@ -32,15 +32,11 @@ export default function App() {
       <form noValidate onSubmit={handleSubmit} className="flex gap-1">
         {/* 하위 커스텀 컴포넌트에 ref 전달하기 (React 18 실패 ❌) */}
         {/* 리액트가 말하길, React.forwardRef 고차 컴포넌트를 사용해야 해! */}
+        {/* 하위 커스텀 컴포넌트에 ref 전달하기 (React 19 성공 ✅) */}
+        {/* 리액트 19 버전부터는 ref는 속성(prop)으로 처리됩니다. */}
         <EmailInput ref={inputRef} />
         <button type="submit" className="button flex gap-1 items-center">
-          <Send
-            ref={(elem) => {
-              console.log(elem)
-            }}
-            size={16}
-          />{' '}
-          제출
+          <Send size={16} /> 제출
         </button>
       </form>
     </LearnSection>
