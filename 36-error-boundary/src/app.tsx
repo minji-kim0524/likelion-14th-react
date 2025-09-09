304b88c26e5e58293cdc1aa3be7830f43c68353e
MINJI <always0524@naver.com>
2025-09-09 21:20:37 +0900

    learn. ErrorBoundary 컴포넌트 2 - 에러 발생 시, 기본 렌더링 될 ErrorFallbackUI 컴포넌트 추출 - 타입 지정 및 내보내기

--- 36-error-boundary/src/app.tsx
+++ 36-error-boundary/src/app.tsx
@@ -1,10 +1,15 @@
 import { LearnSection } from '@/components'
 import RandomCountUp from './demo'
+import ErrorBoundary from './error-boundar'
 
 export default function App() {
+  // throw new Error('App 에러 발생')
+
   return (
     <LearnSection title="랜덤 카운트 업" className="p-10">
-      <RandomCountUp />
+      <ErrorBoundary>
+        <RandomCountUp />
+      </ErrorBoundary>
     </LearnSection>
   )
 }
