79ba6cea91ec081e1756197aaf2206c083495a52
MINJI <always0524@naver.com>
2025-09-09 21:38:10 +0900

    learn. useReducer 훅 활용 - 상태, 액션 타입 지정 - 리듀서 함수 작성 - 액션 정의 - 액션 크리에이터 작성

--- 38-use-reducer/src/counter.tsx
+++ 38-use-reducer/src/counter.tsx
@@ -1,57 +1,43 @@
-import { useState } from 'react'
 import { Minus, Plus } from 'lucide-react'
-import { tw } from './utils'
+import { useCounter } from './hooks'
 
 interface Props {
+  count?: number
   step?: number
   min?: number
   max?: number
 }
 
-export default function Counter({ min = 0, max = 10, step = 1 }: Props) {
-  const [count, setCount] = useState<number>(9)
+export default function Counter({
+  count = 0,
+  min = 0,
+  max = 10,
+  step = 1,
+}: Props) {
+  const countStore = useCounter({ count, min, max, step })
 
   return (
     <div className="flex space-x-6 items-center">
       <button
         type="button"
         aria-label="카운트 감소"
-        disabled={count === min}
-        onClick={() =>
-          setCount((c) => {
-            const nextCount = c - step
-            if (nextCount < min) return min
-            return nextCount
-          })
-        }
-        className={tw(
-          'cursor-pointer select-none',
-          'grid place-content-center',
-          'size-12 border-3 border-white',
-          'text-4xl bg-transparent text-white',
-          'disabled:cursor-not-allowed disabled:border-current disabled:text-gray-600'
-        )}
+        disabled={countStore.isMin}
+        onClick={countStore.minus}
+        className={countStore.buttonStyles}
       >
         <Minus />
       </button>
-      <output className="text-9xl text-white">{count}</output>
+
+      <output className="text-9xl text-white select-none">
+        {countStore.count}
+      </output>
+
       <button
         type="button"
         aria-label="카운트 증가"
-        disabled={count === max}
-        onClick={() =>
-          setCount((c) => {
-            const nextCount = c + step
-            if (nextCount > max) return max
-            return nextCount
-          })
-        }
-        className={tw(
-          'cursor-pointer select-none',
-          'size-12 border-3 border-white',
-          'text-4xl bg-transparent text-white',
-          'disabled:cursor-not-allowed disabled:border-current disabled:text-gray-600'
-        )}
+        disabled={countStore.isMax}
+        onClick={countStore.plus}
+        className={countStore.buttonStyles}
       >
         <Plus />
       </button>
