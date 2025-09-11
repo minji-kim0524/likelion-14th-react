459bc06e389ab9b0302b338d1a9d1ba4c53c9c3d
MINJI <always0524@naver.com>
2025-09-11 23:14:40 +0900

    add. 39-react-context-api 학습 폴더

---
+++ 39-react-context-api/src/app.tsx
@@ -0,0 +1,9 @@
+import { LearnSection, RandomCountUp } from '@/components'
+
+export default function App() {
+  return (
+    <LearnSection title="랜덤 카운트 업">
+      <RandomCountUp />
+    </LearnSection>
+  )
+}
