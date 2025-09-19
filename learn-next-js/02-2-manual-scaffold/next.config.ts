3fbcf0ae10354c245480d185ed55a235716ca5ad
MINJI <always0524@naver.com>
2025-09-19 23:42:10 +0900

    fix. lockfile 파일 충돌 문제 - bun.lock - pnpm-lock.yaml - Next.js(특히 Turbopack)는 프로젝트의 루트를 lockfile을 기준으로 추론 - turbopack.root 직접 지정

--- learn-next-js/02-2-manual-scaffold/next.config.ts
+++ learn-next-js/02-2-manual-scaffold/next.config.ts
@@ -1,20 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // 리액트 엄격 모드 활성화
   reactStrictMode: true,
-
-  // ESLint 설정
   eslint: {
-    // 빌드 시, 린팅 검사 결과 무시 설정
     // ignoreDuringBuilds: false,
   },
-
-  // TypeScript 설정
   typescript: {
-    // 빌드 시, 타입 검사 결과 무시 설정
     // ignoreBuildErrors: false,
   },
-};
+  turbopack: {
+    root: __dirname,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
