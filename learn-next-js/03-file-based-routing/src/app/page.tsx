d6badcd594a034875aa37a73bca80604904c0c6b
MINJI <always0524@naver.com>
2025-09-20 23:04:57 +0900

    learn. Next.js 라우팅 기본

--- learn-next-js/03-file-based-routing/src/app/page.tsx
+++ learn-next-js/03-file-based-routing/src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Page() {
+import { Button } from '@/components'
+
+export default function HomePage() {
   return (
-    <section>
-      <h1>안녕! Next.js</h1>
+    <section className="p-20 flex flex-col gap-y-2 items-start">
+      <h1 className="text-6xl font-thin">홈 페이지</h1>
+      <Button className="px-4 py-1.5 bg-black text-white rounded">
+        로그인
+      </Button>
     </section>
-  );
+  )
 }
