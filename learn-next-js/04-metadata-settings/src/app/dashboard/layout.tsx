544ee235aa7284a54a374c2a968bbf4a4f95a1b5
MINJI <always0524@naver.com>
2025-09-23 22:35:53 +0900

    learn. 메타데이터와 오픈 그래프 태그 설정 - 검색엔진최적화(SEO) 향상 - 사용자 사용성(Usability) 향상

--- learn-next-js/04-metadata-settings/src/app/dashboard/layout.tsx
+++ learn-next-js/04-metadata-settings/src/app/dashboard/layout.tsx
@@ -1,10 +1,22 @@
-import type { PropsWithChildren } from "react";
-import type { Metadata } from "next";
+import type { PropsWithChildren } from 'react'
+import type { Metadata } from 'next'
+import { metadata as RootMetadata } from '@/app/layout'
 
 export const metadata: Metadata = {
-  title: "대시보드 | LearnMate",
-};
+  title: '대시보드 | LearnMate',
+  openGraph: {
+    ...RootMetadata.openGraph,
+    images: [
+      {
+        url: '/og_image_dashboard.png',
+        alt: 'product update 2025',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+}
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
-  return <div className="bg-slate-200 flex-1">{children}</div>;
+  return <div className="bg-slate-200 flex-1">{children}</div>
 }
